bf1c7ba5f38dd3dbc88b84bd362d9a970553ae2d
Geoffrey Yoccoz <geoffrey.yoccoz@gmail.com>
2014-03-04 01:18:52 +0100

    Generate walls

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
 $(document).ready(function()
 {	
-	generateTable(50,150);
+	var w = 150, h = 50;
+
+	generateTable(w,h);
+
+	$('#linkGen').click(function(){
+		randomWalls(w,h);
+	});
 });
 
 
 function generateTable(w,h)
 {
-
 	var table = "";
-	for(var i=0; i<w; i++)
+	for(var i=0; i<h; i++)
 	{
-		console.log("ligne"+i);
 		table += "<tr>";
 
-		for(var j=0; j<h; j++)
+		for(var j=0; j<w; j++)
 		{
-			table += "<td mur='0' class='case' id='line"+i+"-row"+j+"'></td>";
+			table += "<td isWall='0' class='case' id='line"+i+"-row"+j+"'></td>";
 		}
 
 		table += "</tr>";
 	}
 
 	$("#tab").append(table);
+}
+
+function randomWalls(w,h)
+{
+	$('.case').css({"background-color":"blank"});
+	$('.case').attr('isWall','0');
+	var nbWalls = $('#inputWalls').val();
+	if(nbWalls=="")
+	{
+		$('#inputWalls').val('50');
+		nbWalls=50;
+	}
+		
+		
+	//console.log("Nb walls :" + nbWalls);
+	
+	for(i=0; i<nbWalls; i++)
+	{
+		line = Math.floor(Math.random()*h);
+		row = Math.floor(Math.random()*w);
+		
+		//console.log('#line' + line + '-row' + row);
+		$('#line' + line + '-row' + row).css({"background-color":"black"});
+		$('#line' + line + '-row' + row).attr('isWall','1');
+	}
+
 }
